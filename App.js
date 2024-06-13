d1cbb0fe7940d76eb6d0a8b3f72e3578ebd96097
Raghav singh <raghavsinghmanhas184@gmail.com>
2024-06-13 22:56:27 +0530

    changes

--- App.js
+++ App.js
@@ -1,35 +1,21 @@
-/* 
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-<div id="parent">
-  <div id=child>
-    <h1>I'm h1 Tag</h1>
-    <h2>I'm h2 Tag</h2>
-  </div>
-  <div id=child2>
-    <h1>I'm h1 Tag</h1>
-    <h2>I'm h2 Tag</h2>
-  </div>
-</div>
+const parent = React.createElement('div', { id: "parent" }, [
+  React.createElement('div', { id: "child" }, [
+    React.createElement('h1', {}, "I'm h1 Tag"),
+    React.createElement('h2', {}, "I'm h2 Tag"),
+  ]),
+  React.createElement('div', { id: "child2" }, [
+    React.createElement('h1', {}, "I'm H1 Tag"),
+    React.createElement('h2', {}, "I'm h2 Tag"),
+  ]),
+]);
 
-ReactElement(Object) => HTML(Browser Understands)
+console.log(parent); // object
 
-*/
+const root = createRoot(document.getElementById("root"));
+root.render(parent);
 
-  const parent = React.createElement('div', { id: "parent" }, [
-    React.createElement('div', { id: "child" }, [
-      React.createElement('h1', {}, "I'm h1 Tag"),
-      React.createElement('h2', {}, "I'm h2 Tag"),
-    ]),
-    React.createElement('div', { id: "child2" }, [
-      React.createElement('h1', {}, "I'm h1 Tag"),
-      React.createElement('h2', {}, "I'm h2 Tag"),
-    ]),
-  ]);
-  //JSX
-  console.log(parent); // object
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  
-  root.render(parent);
-  
 
   
---
+++ README.md
@@ -0,0 +1,17 @@
+#Namaste-React
+
+#Parcel
+-Dev Build
+-Local server
+-HMR -hot module Replacement
+-caching
+-Image optimization
+-Minification
+-Building 
+-Compressing
+-Tree shaking
+-Differen dev and production bundlers
+-consistent Hashing
+-Code Splitting
+
+
